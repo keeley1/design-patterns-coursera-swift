a1810e333e343f8ba92fb34c89a1d81c56c0b02d
keeley1 <keeleygardner.gardner@gmail.com>
2025-05-04 20:51:06 +0100

    Updating factory example

--- factory-method-pattern/factory-method-pattern/ViewModel/CupViewModel.swift
+++ factory-method-pattern/factory-method-pattern/ViewModel/CupViewModel.swift
@@ -1,5 +1,14 @@
 import Foundation
 
 final class CupViewModel {
-    
+    // Define a method to create a cup using the factory
+    func createCup() {
+        // Pass in data to create a
+        let cup = CupFactory.createCup(id: 1,
+                                       isGlassEmpty: true,
+                                       cupSize: .litre,
+                                       type: .glass(isClear: false))
+        // Factory should know to create a "glass cup" object
+        print("Cup details: ", cup)
+    }
 }
